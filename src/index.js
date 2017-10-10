ed2b61c50c4361bab96e74287da2d235e835eb1e
Tom Burnett <t_burnett@outlook.com>
2017-10-11 00:39:39 +0100

    Remove test data

--- src/index.js
+++ src/index.js
@@ -5,37 +5,12 @@ import 'normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 
 import AppRouter from './routers/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const store = configureStore();
-
-store.dispatch(
-  addExpense({
-    description: 'Water bill',
-    amount: 10000,
-    createdAt: 12345
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: 'Gas bill',
-    amount: 5000,
-    createdAt: 12346
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: 'Rent',
-    amount: 50000,
-    createdAt: 123
-  })
-);
 
 const jsx = (
   <Provider store={store}>
